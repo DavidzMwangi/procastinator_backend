0ad7cd230a6ed7b965f87220015852ba6a119c43
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-12-03 08:53:27 +0300

    login api

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('hello',function (){
+   return json_encode(\App\User::all());
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace'=>'Api'],function () {
+
+    Route::group(['namespace' => 'Auth'], function () {
+        Route::post('login', 'LoginController@login');
+        Route::post('register', 'RegisterController@register');
+
+        Route::post('recover_password', 'Login2Controller@recoverPassword');
+    });
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
 });
